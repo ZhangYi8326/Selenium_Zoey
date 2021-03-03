6014b83cc0ffdb0970d82c1be719a031f6353eeb
ZhangYi8326 <891758726@qq.com>
2021-03-03 18:19:38 +0800

    用例逻辑更改

--- seleniumPO/testcase/test_contact.py
+++ seleniumPO/testcase/test_contact.py
@@ -6,15 +6,29 @@
 
 from seleniumPO.page.main_page import MainPage
 from seleniumPO.page.login_page import LoginPage
+from seleniumPO.pyse.driver import Driver
+from seleniumPO.page.index_page import IndexPage
+from seleniumPO.page.add_member_page import AddMemberPage
 
 
 class TestContact:
-    def setup(self):
-        # self.mainpage = MainPage()
-        self.loginpage = LoginPage()
 
-    def teardown_class(self):
-        self.mainpage.quit_driver()
+    def setup_class(self):
+        self.driver = Driver()
+        self.loginpage = LoginPage(self.driver)
+        self.index = IndexPage(self.driver)
+        self.mainpage = MainPage(self.driver)
+        self.addmember = AddMemberPage(self.driver)
+
+    def setup_class(self):
+        self.driver.quit()
+
+    def test_index(self):
+        """
+        从首页进入登录页面
+        :return:
+        """
+        self.index.goto_login()
 
     def test_login(self):
         """
@@ -23,10 +37,21 @@ class TestContact:
         """
         self.loginpage.scan()
 
+    def test_goto_addmember_page(self):
+        """
+        从主页进入通讯录页面
+        :return:
+        """
+        self.mainpage.goto_add_member()
+
     def test_addmember(self):
+        """
+        添加成员
+        :return:
+        """
         username = "u222"
         account = "a222"
         phone = "11122222222"
-        page = self.mainpage.goto_add_member()
-        names = page.get_member()
+        self.addmember.add_member(username, account, phone)
+        names = self.addmember.get_member()
         assert username in names
